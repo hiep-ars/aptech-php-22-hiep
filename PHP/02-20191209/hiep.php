70559419e3eee7e8e6967a7370f80ffd8dc4746e
hiep-ars <ltruonghiep37@gmail.com>
2019-12-09 21:21:39 +0700

    Update

---
+++ PHP/02-20191209/hiep.php
@@ -0,0 +1,28 @@
+<?php
+echo "ahah";
+echo "<br>";
+$hiep = 2019;
+$haha = 1997;
+echo $hiep - $haha;
+$php = ["nam", "hiep", "binh", "hoang"];
+echo $php[2];
+?>
+<h1>
+    <?php
+    echo ("huhu");
+    ?>
+</h1>
+<?php
+ $products = [
+    "iphone 11" => [
+      "name" => "iPhone 11 Sieu Dep",
+      "price" => 999.99
+    ],
+    "s10" => [
+      "name" => "Galaxy S10",
+      "price" => 998.98
+    ]
+  ];
+  echo $products["iphone 11"]["name"];
+  ?>
+
