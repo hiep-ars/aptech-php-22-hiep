d5b502b9117f96472f0c1529f3df165d4d16286b
hiep-ars <ltruonghiep37@gmail.com>
2019-12-09 17:49:31 +0700

    aaa

--- PHP/01-20161206/index.php
+++ PHP/01-20161206/index.php
@@ -10,8 +10,8 @@
 <title>Title</title>
 </head>
 <body>
-<h1>hashaaaaaaaaaaaaaaaa</h1>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+<h1>hashaaaaaaaaaaaaaa</h1>
+<script src="https://cjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
